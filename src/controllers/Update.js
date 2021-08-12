5f5d424ef2950da2487d129f4e08055d0f9714a1
Hanif Adams <omeizahanif@gmail.com>
2021-08-12 15:34:01 +0100

    added createCardMsg and case statement

--- src/controllers/Update.js
+++ src/controllers/Update.js
@@ -1,7 +1,73 @@
 import R from 'ramda';
 
+const MSGS = {
+    SHOW_FORM: 'SHOW_FORM',
+    QUESTION_INPUT: 'QUESTION_INPUT',
+    ANSWER_INPUT: 'ANSWER_INPUT',
+    CREATE_CARD: 'CREATE_CARD',
+}
+
+export function showFormMsg(showCardForm) {
+    return {
+        type: MSGS.SHOW_FORM,
+        showCardForm
+    }
+}
+
+export function saveQuestionInput(question) {
+    return {
+        type: MSGS.QUESTION_INPUT,
+        question
+    }
+}
+
+export function saveAnswerInput(answer) {
+    return {
+        type: MSGS.ANSWER_INPUT,
+        answer
+    }
+}
+
+export function createCardMsg(question, answer) {
+    return {
+        type: MSGS.CREATE_CARD,
+        question,
+        answer
+    }
+}
+
 function update(msg, model) {
-    return model;
+    switch(msg.type) {
+        case MSGS.SHOW_FORM: {
+            const { showCardForm } = msg;
+            return { ...model, showCardForm, question: "" };
+        }
+            
+        case MSGS.QUESTION_INPUT: {
+            const { question } = msg;
+            return { ...model, question }
+        }
+
+        case MSGS.ANSWER_INPUT: {
+            const { answer } = msg;
+            return { ...model, answer }
+        }
+
+        case MSGS.CREATE_CARD: {
+            const { question, answer } = msg;
+            const { cards, nextID } = model;
+            cards.push({
+                question,
+                answer,
+                id: nextID,
+                showAnswer: false,
+                rating: null,
+                showButtons: false
+            })
+            return { ...model, cards, question: '', answer: '', nextID: nextID + 1, showCardForm: false }
+        }
+
+    }
 }
 
 export default update;
